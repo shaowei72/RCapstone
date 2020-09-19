affe93fdba1b24cb2ff7c06aff2a20fdc06b608d
shaowei72 <shaowei.ying@gmail.com>
2020-09-19 10:18:38 +0800

    TravisTrigger12

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,7 +1,7 @@
 library(testthat)
-library(Capstone)
+# library(Capstone)
 
-Sys.setenv(R_TESTS="")
+# Sys.setenv(R_TESTS="")
 
 # testthat::test_dir(path = "./tests/testthat/")
 # testthat::test_dir("tests/testthat/")
